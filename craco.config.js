6457d602a7e339b61276e52fe1b97b442fc522a8
Leonardo R <leonardo.stacowski@hotmail.com>
2021-07-03 04:08:18 -0300

    Adição de variável de ambiente

--- craco.config.js
+++ craco.config.js
@@ -4,6 +4,7 @@ const webpack = require('webpack')
 const APP_NAME = 'GIT Search'
 const CLIENT_ID = 'bfd5bd9e77a0636353c1'
 const CLIENT_ID_SECRET = '42d1c96ed2cc5903e13b3628ec4614787541e09c'
+const NODE_ENV = process.env.NODE_ENV || 'production'
 
 module.exports = {
   webpack: {
@@ -16,6 +17,7 @@ module.exports = {
           APP_NAME: JSON.stringify(APP_NAME),
           CLIENT_ID: JSON.stringify(CLIENT_ID),
           CLIENT_ID_SECRET: JSON.stringify(CLIENT_ID_SECRET),
+          NODE_ENV: JSON.stringify(NODE_ENV),
         },
       }),
     ],
